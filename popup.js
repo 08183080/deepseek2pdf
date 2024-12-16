c541d364c8126027723209b5554a8afe805013c8
08183080 <08183080@cumt.edu.cn>
2024-12-16 19:17:25 +0800

    ctrl+p

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.getElementById('printPage').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        function: printPage
+      });
+    });
+  });
+  
+  function printPage() {
+    window.print();
+  }
